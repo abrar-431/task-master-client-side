a29fde042a52661c44e91a55a308e69ee7ecd702
abrar-431 <abrarh1520444@gmail.com>
2024-06-19 07:00:48 -0700

    title component added

---
+++ src/Layout/Home/Features/Features.jsx
@@ -0,0 +1,11 @@
+import Title from "../../../Components/Title";
+
+const Features = () => {
+    return (
+        <div className="mt-10">
+            <Title heading={"Features"} subHeading="Discover the Benefits of Using TaskMaster"></Title>
+        </div>
+    );
+};
+
+export default Features;
